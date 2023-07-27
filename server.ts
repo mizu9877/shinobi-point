2ca7f6a168b1909b7451dbcab35e97a58978453b
mizu9877 <satoshi.a9877@gmail.com>
2023-07-27 07:31:38 +0200

    feat: keep going with typescript

--- server.ts
+++ server.ts
@@ -1,10 +1,14 @@
-const express = require('express')
+import express from 'express'
+import cors from 'cors'
+
+
 const router = express.Router()
 
 const app = express()
 
 const users = require('./routes/api/user')
 
+app.use(cors())
 app.use('/api/users', users)
 
 
