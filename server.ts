58421eea135d798e2ef38c629176e2b000eac4ed
mizu9877 <satoshi.a9877@gmail.com>
2023-08-01 09:40:14 +0200

    fix: all of files as typescript.

--- server.ts
+++ server.ts
@@ -1,16 +1,12 @@
-import express from 'express'
-import cors from 'cors'
+import express from 'express';
 
+const app = express();
+const port = process.env.PORT || 5000;
 
-const router = express.Router()
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
 
-const app = express()
-
-const users = require('./routes/api/user')
-
-app.use(cors())
-app.use('/api/users', users)
-
-
-const PORT = process.env.PORT || 5000
-app.listen(PORT, () => console.log(`Server is running on port : ${PORT}`))
+app.listen(port, () => {
+    console.log(`Server is running on port:${port}`);
+});
