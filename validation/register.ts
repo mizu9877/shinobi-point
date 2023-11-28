9f0f4574dfb553c35fb016921e18a8c3f4b249ea
mizu9877 <satoshi.a9877@gmail.com>
2023-11-29 01:32:30 +0900

    add: register validation not complete

--- validation/register.ts
+++ validation/register.ts
@@ -1,2 +1,20 @@
-import validator from 'validator'
-const isEmpty = require("./is-empty")
+import Validator from "validator";
+const isEmpty = require("./is-empty");
+
+const validateRegisterInput = (data) => {
+  let errors = {};
+
+  // Need to check the each item is empty...
+  data.name = !isEmpty(data.name) ? data.name : "";
+  data.email = !isEmpty(data.email) ? data.email : "";
+  data.password = !isEmpty(data.password) ? data.password : "";
+  data.password2 = !isEmpty(data.password2) ? data.password2 : "";
+
+  if (Validator.isLength(data.name, { min: 2, max: 30 })) {
+    errors.name = "Name must be between 2 and 30 characters";
+  }
+
+  if (Validator.isEmpty(data.name)) {
+    errors.name = "Name field is required";
+  }
+};
