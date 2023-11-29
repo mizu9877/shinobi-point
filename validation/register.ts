61a32d0fc4b5a3f1f80f173b4f9e04d4dd288c96
mizu9877 <satoshi.a9877@gmail.com>
2023-11-29 14:41:55 +0900

    update: validation register complete

--- validation/register.ts
+++ validation/register.ts
@@ -17,4 +17,35 @@ const validateRegisterInput = (data) => {
   if (Validator.isEmpty(data.name)) {
     errors.name = "Name field is required";
   }
+
+  if (Validator.isEmpty(data.email)) {
+    errors.email = "Email field is required";
+  }
+
+  if (!Validator.isEmail(data.email)) {
+    errors.email = "Email is invalid";
+  }
+
+  if (Validator.isEmpty(data.password)) {
+    errors.password = "Password field is required";
+  }
+
+  if (!Validator.isLength(data.password, { min: 6, max: 30 })) {
+    errors.password = "Password must be at least 6 characters";
+  }
+
+  if (Validator.isEmpty(data.password2)) {
+    errors.password2 = "Confirm password field is required";
+  }
+
+  if (!Validator.equals(data.password, data.password2)) {
+    errors.password2 = "Passwords must match";
+  }
+
+  return {
+    errors,
+    isValid: isEmpty(errors),
+  };
 };
+
+module.exports = validateRegisterInput;
